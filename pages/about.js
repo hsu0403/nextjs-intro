4d0962ff084924c33e2adb9b8a1dc18b5b65ec60
SeongUn <hseongun0403@gmail.com>
2022-04-19 20:34:40 +0900

    nextjs

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Seo from "../components/Seo";
+
+export default function About() {
+  return (
+    <>
+      <Seo title="About" />
+      <div>about</div>
+    </>
+  );
+}
